656d303f8cb0c3b1c901a7b9cb9a13e2ec9007a7
VasckMe <kasarin.anton@gmail.com>
2022-08-26 15:56:04 +0300

    Project created

---
+++ Animation App/Animation App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animation App
+//
+//  Created by Apple Macbook Pro 13 on 26.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
